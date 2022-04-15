fbd88058b10ff01a578c74ef0940d32497dd4e41
Ziyodullokhon <studiovisual0077@gmail.com>
2022-04-15 18:53:14 -0400

    8

--- netflix/settings.py
+++ netflix/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'netflix.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB'),
+        'NAME': os.environ.get('POSTGRES_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': 'localhost',
